99eff0947b4ba3a529d30ce7965c24a2945ebe41
Razu Ahmed Joy <rjjoybzs420@gmail.com>
2021-05-19 22:54:52 +0600

    first production by Razu

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const footerSection = document.getElementById("jsfoot");
+const footerText = ` <div class="text-right container">
+            <p class="text-end">
+                Built with ❤ by <a class="text-light" href="https://prowebpark.com">prowebpark.com</a>
+            </p>
+        </div>`;
+footerSection.innerHTML = footerText;
